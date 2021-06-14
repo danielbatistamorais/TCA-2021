eaf170531e04ceb56d7fc8f5a90ce4c460a1ab4e
luiz.nunes <luiz_nunes15@hotmail.com>
2021-06-14 18:20:45 -0300

    eita

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Raffle from 'App/Models/Raffle'
 import Ticket from 'App/Models/Ticket'
@@ -5,14 +6,24 @@ import User from 'App/Models/User'
 
 export default class HomeController {
   public async index({ view, auth }: HttpContextContract) {
-    const raffles = await Raffle.all()
-    const rafflesPartic = await Raffle.query().whereHas('tickets', (query) => {
-      query.where('user_id', auth.user!!.id)
-    })
-    const users = await User.query()
-    const tickets = await Ticket.query()
+    if (auth.user!!) {
+      const raffles = await Raffle.all()
 
-    return view.render('home/index', { raffles, rafflesPartic, users, tickets })
+      const rafflesPartic = await Raffle.query().whereHas('tickets', (query) => {
+        query.where('user_id', auth.user!!.id)
+      })
+      const users = await User.query()
+      const tickets = await Ticket.query()
+  
+      return view.render('home/index', { raffles, rafflesPartic, users, tickets })
+    }
+    else {
+      const raffles = await Raffle.all()
+      const users = await User.query()
+      const tickets = await Ticket.query()
+  
+      return view.render('home/index', { raffles, users, tickets })
+    }
   }
 
   public async about({ view }: HttpContextContract) {
