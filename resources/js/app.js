f65c701ff6236488323aaee0a724706f443aa5fa
luiz.nunes <luiz_nunes15@hotmail.com>
2021-06-16 19:24:53 -0300

    premio att

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,35 @@
+/* eslint-disable prettier/prettier */
 import '../css/app.scss'
 import 'bootstrap'
+
+try {
+    const divPremios = document.querySelector('.premios')
+    const btAdicionar = document.querySelector('.adicionar')
+  
+    btAdicionar.addEventListener('click', () => {
+      const div1 = document.createElement('div')
+      const div2 = document.createElement('div')
+      const input = document.createElement('input')
+      const input2 = document.createElement('input')
+  
+      divPremios.firstChild
+  
+      div1.classList.add('col-6')
+      div1.classList.add('mb-3')
+      
+      div2.classList.add('col-6')
+      div2.classList.add('mb-3')
+  
+      input.classList.add('form-control')
+      input.type = `text`
+
+      input2.classList.add('form-control')
+      input2.type = `text`
+  
+      div1.appendChild(input)
+      div2.appendChild(input2)
+      
+      divPremios.appendChild(div1)
+      divPremios.appendChild(div2)
+    })
+} catch (error) {}
